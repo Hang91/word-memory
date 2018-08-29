5ab5e492669c351acaa1cdd2e010687b98542da6
Hang Jin <hangjin@Hangs-MBP.fios-router.home>
2018-08-29 10:39:25 -0400

    implement search function and http response with json function

---
+++ models.go
@@ -0,0 +1,28 @@
+package main
+
+import "gopkg.in/mgo.v2/bson"
+
+//User is the stuct to record user information,
+//user account password,
+//user search history
+type User struct {
+	ID            bson.ObjectId `bson:"_id" json:"id"`
+	FirstName     string        `bson:"first_name" json:"first_name"`
+	LastName      string        `bson:"last_name" json:"last_name"`
+	SearchHistory []string      `bson:"search_history" json:"search_history"`
+	Email         string        `bson:"email" json:"email"`
+	Password      string        `bson:"password" json:"password"`
+}
+
+// Word is the stuct to record word,
+// meaning and part of speech of the word, level of the word
+// Level of a word is a label to mark the frequency of searchcing,
+// difficulty of a word
+// Meaning is a map. The key of the map is part of speech,
+// the value is possible meanings under this part.
+type Word struct {
+	ID      bson.ObjectId       `bson:"_id" json:"id"`
+	Spell   string              `bson:"spell" json:"spell"`
+	Meaning map[string][]string `bson:"meaning" json:"meaning"`
+	Level   int                 `bson:"level" json:"level"`
+}
